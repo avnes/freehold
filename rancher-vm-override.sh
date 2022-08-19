73e9b514a7881c4e46691209e8dec037dfccbc27
Audun Nes <audun.nes@gmail.com>
2022-08-19 21:58:18 +0200

    New overide file for Rancher VMs
    Signed-off-by: Audun Nes <audun.nes@gmail.com>

---
+++ rancher-vm-override.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+nmcli con mod "System eth0" ipv4.dns "10.0.1.74 10.0.0.1 8.8.8.8"
+systemctl restart NetworkManager
+
+cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+EOF
+
+sysctl --system
+
+setenforce 0
+sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+dnf update -y
+
+dnf remove -y buildah
+
+dnf remove -y docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+
+dnf install -y yum-utils
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+dnf install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+dnf install -y git
+
+systemctl enable docker
+systemctl start docker
